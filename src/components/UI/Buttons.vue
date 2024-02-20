b0e62cbb3e8b426cd954949c760511e05e6d2894
Michaelle Valenzuela <michaelle.valenzuela@epn.edu.ec>
2024-02-20 13:39:06 -0500

    Despliegue frontend

---
+++ src/components/UI/Buttons.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: ["type_btn", "btn_align", "classApply", "btn_text", "isIcon", "isEvent"],
+    components: {},
+});
+</script>
+
+<template>
+    <div :style="{ textAlign: btn_align, marginTop: '3%', marginBottom: '3%' }">
+
+        <button v-if="!isEvent" :class="[classApply, 'py-2 px-4 rounded']" :type="type_btn">
+            <i v-if="isIcon" :class="[btn_text]"></i>
+            {{ !isIcon ? btn_text : "" }}
+        </button>
+        <button v-else :class="[classApply, 'py-2 px-4 rounded']" :type="type_btn">
+            <i v-if="isIcon" :class="[btn_text]"></i>
+            {{ !isIcon ? btn_text : "" }}
+        </button>
+
+    </div>
+</template>
---
+++ src/components/UI/LabelInputs.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: ["classLabel", "label_name", "label_for_to_id"],
+    components: {},
+});
+</script>
+
+<template>
+    <label :class="classLabel" :for="label_for_to_id">
+        {{ label_name }}
+    </label>
+</template>
